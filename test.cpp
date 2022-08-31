5625300ef870463d2b538fd1aae944ecb15d9dc6
PanyCG <panycg_cn@163.com>
2022-08-31 16:41:00 +0800

    The second commit!

---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std; // make definitions visible
+
+int main()  // function header
+{           // start of function body 
+    cout << "Hello C++!" << endl; // output a message
+    return 0; // terminate main()
+}
