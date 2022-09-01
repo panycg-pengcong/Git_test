c8f410633a8752c82fad37a94ae6db1995a535ea
PanyCG <panycg_cn@163.com>
2022-09-01 20:38:14 +0800

    The fourth commit!

--- test.cpp
+++ test.cpp
@@ -4,5 +4,7 @@ using namespace std; // make definitions visible
 int main()  // function header
 {           // start of function body 
     cout << "Hello C++!" << endl; // output a message
+    cout << "Hi!" << endl;
+    cout << endl;
     return 0; // terminate main()
 }
